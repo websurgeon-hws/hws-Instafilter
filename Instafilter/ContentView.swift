566dc87bf56edc438c27419721b8d8cfb80da62b
Peter Barclay <peter@peter-barclay.com>
2019-11-25 08:15:28 +0000

    Display Image

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -5,24 +5,19 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingActionSheet = false
-    @State private var backgroundColor = Color.white
+    @State private var image: Image?
 
     var body: some View {
-        Text("Hello, World!")
-            .frame(width: 300, height: 300)
-            .background(backgroundColor)
-            .onTapGesture {
-                self.showingActionSheet = true
-            }
-            .actionSheet(isPresented: $showingActionSheet) {
-                ActionSheet(title: Text("Change background"), message: Text("Select a new color"), buttons: [
-                    .default(Text("Red")) { self.backgroundColor = .red },
-                    .default(Text("Green")) { self.backgroundColor = .green },
-                    .default(Text("Blue")) { self.backgroundColor = .blue },
-                    .cancel()
-                ])
-            }
+        VStack {
+            image?
+                .resizable()
+                .scaledToFit()
+        }
+        .onAppear(perform: loadImage)
+    }
+
+    func loadImage() {
+        image = Image("Example")
     }
 }
 
