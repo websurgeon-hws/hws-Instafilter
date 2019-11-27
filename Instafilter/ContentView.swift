078c9fc4f21a49dcf214c7d625951a09fdada1c4
Peter Barclay <peter@peter-barclay.com>
2019-11-27 08:39:02 +0000

    Importing an image into SwiftUI using UIImagePickerController

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -7,7 +7,9 @@ import SwiftUI
 struct ContentView: View {
     @State private var image: Image?
     @State private var filterIntensity = 0.5
-    
+    @State private var showingImagePicker = false
+    @State private var inputImage: UIImage?
+
     var body: some View {
         NavigationView {
             VStack {
@@ -26,7 +28,7 @@ struct ContentView: View {
                     }
                 }
                 .onTapGesture {
-                    // select an image
+                    self.showingImagePicker = true
                 }
                 
                 HStack {
@@ -48,7 +50,16 @@ struct ContentView: View {
             }
             .padding([.horizontal, .bottom])
             .navigationBarTitle("Instafilter")
+            .sheet(isPresented: self.$showingImagePicker, onDismiss: loadImage) {
+                ImagePicker(image: self.$inputImage)
+            }
         }
+    }
+    
+    func loadImage() {
+        guard let inputImage = inputImage else { return }
+
+        image = Image(uiImage: inputImage)
     }
 }
 
